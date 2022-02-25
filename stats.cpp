98b95bf6a804443615857023fa9532e493b95e32
arpit091429 <arpitkhandelwal24@gmail.com>
2022-02-25 12:53:01 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,22 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+#include<bits/stdc++.h>
+
+template<typename T> Statistics::Stats<>::Stats(){}
+template<typename T> Statistics::Stats<T,T,T>::Stats(T average,T max,T min):average(average),max(max),min(min){}
+template<typename T> Statistics::Stats<T> Statistics::ComputeStatistics(const std::vector<T>& data)
+{
+    Stats<T> st;
+    if(data.size()==0)
+    {
+       st.average=NAN;
+       st.max=NAN;
+       st.min=NAN;
+    }
+    
+    st.max=*max_element(data.begin(),data.end());
+    st.min=*min_element(data.begin(),data.end());
+    T sum=accumulate(data.begin(), data.end() , 0);
+    st.average=(sum/data.size());
+    return st;  
 }

b755f626b3866ad11d7fa9dab3627536a7a8f0f7
arpit091429 <arpitkhandelwal24@gmail.com>
2022-02-25 15:57:08 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,21 +2,32 @@
 
 #include<bits/stdc++.h>
 
-//template<typename T> Statistics::stats(){}
-template<typename T> Statistics::stats<T>::stats(T average,T max,T min):average(average),max(max),min(min){}
-template<typename T> Statistics::stats<T> Statistics::ComputeStatistics(const std::vector<T>& data)
+//template<typename T> Statistics::Stats<>::Stats(){}
+//template<typename T> Statistics::Stats<T,T,T>::Stats(T Average,T Max,T Min):Average(Average),Max(Max),Min(Min){}
+//template<typename T> Statistics::Stats<T> Statistics::ComputeStatistics(const std::vector<T>& data)
+Statistics::Stats::Stats(){}
+Statistics::Stats::Stats(float Average,float Max,float Min):Average(Average),Max(Max),Min(Min){}
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& data)
+
 {
-    stats<T> st;
+    Stats<float> st;
     if(data.size()==0)
     {
-       st.average=NAN;
-       st.max=NAN;
-       st.min=NAN;
+       st.Average=NAN;
+       st.Max=NAN;
+       st.Min=NAN;
     }
     
-    st.max=*max_element(data.begin(),data.end());
-    st.min=*min_element(data.begin(),data.end());
-    T sum=accumulate(data.begin(), data.end() , 0);
-    st.average=(sum/data.size());
-    return st;  
+    st.Max=*max_element(data.begin(),data.end());
+    st.Min=*min_element(data.begin(),data.end());
+    float sum=accumulate(data.begin(), data.end() , 0);
+    st.Average=(sum/data.size());
+    return st;
+   // float maxi=*max_element(data.begin(),data.end());
+    //float mini=*min_element(data.begin(),data.end());
+    //float sum=accumulate(data.begin(), data.end() , 0);
+    //float avg=sum/data.size();
+    //return Statistics::Stats(avg,maxi,mini);
+
+    
 }

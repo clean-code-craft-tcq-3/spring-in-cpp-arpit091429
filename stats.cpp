3ebd7d4be8913c44bf80ab43379f0c6eaf4cc4ca
arpit091429 <arpitkhandelwal24@gmail.com>
2022-02-25 16:01:01 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -6,22 +6,22 @@
 //template<typename T> Statistics::Stats<T,T,T>::Stats(T Average,T Max,T Min):Average(Average),Max(Max),Min(Min){}
 //template<typename T> Statistics::Stats<T> Statistics::ComputeStatistics(const std::vector<T>& data)
 Statistics::Stats::Stats(){}
-Statistics::Stats::Stats(float Average,float Max,float Min):Average(Average),Max(Max),Min(Min){}
+Statistics::Stats::Stats(float Average,float Max,float Min):average(Average),max(Max),min(Min){}
 Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& data)
 
 {
     Stats<float> st;
     if(data.size()==0)
     {
-       st.Average=NAN;
-       st.Max=NAN;
-       st.Min=NAN;
+       st.average=NAN;
+       st.max=NAN;
+       st.min=NAN;
     }
     
-    st.Max=*max_element(data.begin(),data.end());
-    st.Min=*min_element(data.begin(),data.end());
+    st.max=*max_element(data.begin(),data.end());
+    st.min=*min_element(data.begin(),data.end());
     float sum=accumulate(data.begin(), data.end() , 0);
-    st.Average=(sum/data.size());
+    st.average=(sum/data.size());
     return st;
    // float maxi=*max_element(data.begin(),data.end());
     //float mini=*min_element(data.begin(),data.end());
